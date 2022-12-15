9d679a98e78846415afd95dd69393fe22d1ed5bf
Adam Klie <aklie@eng.ucsd.edu>
2022-12-15 14:48:24 -0800

    first commit for working more directly on interpret

---
+++ preprocess/__init__.py
@@ -0,0 +1,43 @@
+from ._seq_preprocess import (
+    sanitize_seq,
+    sanitize_seqs,
+) # sanitize seqs
+
+from ._seq_preprocess import (
+    ascii_encode_seq,
+    ascii_encode_seqs,
+    ascii_decode_seq,
+    ascii_decode_seqs
+) # ascii encode/decode
+
+from ._seq_preprocess import (
+    reverse_complement_seq,
+    reverse_complement_seqs,
+)  # reverse complement
+
+from ._seq_preprocess import (
+    ohe_seq,
+    ohe_seqs,
+    decode_seq,
+    decode_seqs,
+)  # one-hot encode
+
+from ._seq_preprocess import (
+    dinuc_shuffle_seq,
+    dinuc_shuffle_seqs,
+)  # dinucleotide shuffle
+
+from ._seq_preprocess import (
+    perturb_seq,
+    perturb_seqs
+) # perturb
+
+from ._seq_preprocess import (
+    feature_implant_seq, 
+    feature_implant_across_seq
+) # feature implant
+
+from ._seq_preprocess import (
+    gc_content_seq, 
+    gc_content_seqs
+) # gc
